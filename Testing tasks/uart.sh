38e437c8c74e6e6bdf20639c2017095c5ae1dd09
Matej Jech <matej.jech@workswell.cz>
2020-08-17 15:38:38 +0200

    UART now exits on invalid baud rate

--- Testing tasks/uart.sh
+++ Testing tasks/uart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 [ $# -ne 1 ] && echo "Usage: $0 <baud speed>" && exit 1
 i=0
-( stty $1 < /dev/ttyUSB1; cat;
+( stty $1 < /dev/ttyUSB1;
+  [ $? -ne 0 ] && echo "Invalid baud rate" && exit 1
+  cat;
   while [ $i -ne 10 ]
   do
     echo "Receiving packet $i"
